df05266034838c74ffadfe0235eadf98c2b8692c
Geomty <69478549+Geomty@users.noreply.github.com>
2023-08-05 21:00:31 -0400

    add instrumented test

--- app/src/androidTest/java/com/geomty/helloworld/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/geomty/helloworld/ExampleInstrumentedTest.kt
@@ -1,24 +1,24 @@
 package com.geomty.helloworld
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.geomty.helloworld", appContext.packageName)
+    fun spamButton() {
+        for (i in 1..50) {
+            onView(withId(R.id.tap_me_button)).perform(click())
+        }
     }
 }
